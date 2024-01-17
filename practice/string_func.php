79536fd98122abed83b7bc353b70b911500b23f3
Preyansh Patel <patelpreyansh3011@gmail.com>
2024-01-17 18:05:05 +0530

    string func

--- practice/string_func.php
+++ practice/string_func.php
@@ -7,11 +7,12 @@ $a_tag_string = '<a href="https://www.google.com">Go to Google.com</a>';
 
 // echo strlen($x);
 
-// echo str_replace("Hello","Hyy",$x);
+// echo str_replace("l","b",$x);
 
 // echo strpos($x,"r");
 
 // echo substr($x,4,5);
+// echo substr($x,-3,4);
 
 // echo strtoupper($lower_case_string);
 
@@ -23,10 +24,15 @@ $a_tag_string = '<a href="https://www.google.com">Go to Google.com</a>';
 
 // print_r(explode(" ","Hello World"));
 
+// echo $html_string;
 // echo htmlspecialchars($html_string);
+// htmlspecialchars() and our improved modification p() and s() are used in forms and displaying of nonhtml text.
 
+// echo $a_tag_string;
 // echo htmlentities($a_tag_string);
+// htmlentities() is used to convert given text into ASCII encoding;
 
+// Insert html line break
 // echo nl2br("Hello world1\nHello world2");
 
 // echo str_repeat("Hello world\n",2);
@@ -40,31 +46,35 @@ $a_tag_string = '<a href="https://www.google.com">Go to Google.com</a>';
 // echo str_word_count($x);
 // echo str_word_count($html_string);
 
-// echo substr_replace($x,"Byy",0,5);
+// echo substr_replace($x,"Byy",0,7);
 
 // echo str_pad($x,20,'.');
 
-
 // What is binary safe string comparison?
 // echo strcmp($x,"Hello World");
+// echo strcmp($x,"Hello world");
 // echo strcmp($x,"Hello World234");
 // echo strcmp($x,"Hello");
-
+// echo strcmp("50000000000000","0");
 
 // What is locale based string comparison?
 // echo strcoll($x,"Hello World");
 // echo strcoll($x,"Hello World234");
 // echo strcoll($x,"Hello");
 
-
-// echo strcspn($x,"r");
-
+// echo strcspn($x,"W",0,10);
 
 // echo stristr($x,"world");
 // echo stristr("Hello world hello! hello world", "WORLD");
+// echo stristr("Hello world hello! hello world", "ll");
 
+// echo strpbrk($x,"ll");
+// echo strpbrk($x,"ab");
 
-// echo strpbrk($x,"r");
+// echo strstr($x, "ll");
 
+// echo strtr($x, 'l','.');
 
-// echo strstr($x, "Wo");
+// echo ucfirst("abcd");
+
+echo ucwords("abcd xyz mnop 123 @34");
