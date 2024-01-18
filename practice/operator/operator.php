befdb182c23d792319e0aa96964aeb7044397713
Preyansh Patel <patelpreyansh3011@gmail.com>
2024-01-18 17:49:03 +0530

    datatype, math function and operator added

---
+++ practice/operator/operator.php
@@ -0,0 +1,140 @@
+<?php
+
+// Arithmetic Operator
+
+// echo 15+20;
+// var_dump(15.35+20.65);
+
+// echo 15-20;
+// echo 20-5;
+// echo 15.32-56.89;
+// var_dump(54.69-36.58);
+
+// echo 5*9;
+// echo 5.1*0;
+// echo 5.1*-2;
+// var_dump(5.1*6.9);
+
+// echo 10/2;
+// echo 15/8;
+// var_dump(10/2);
+// var_dump(58/9);
+// var_dump(58/9.6);
+// var_dump(58/0); // Division by zero error
+
+// echo 10%2;
+// var_dump(10%2);
+// echo 78%5;
+// var_dump(78%5);
+// can't give float to modulus operator
+
+// echo 10**2;
+// var_dump(10**2);
+// echo 5**0;
+// var_dump(5**0);
+// echo 5**6.5;
+// var_dump(5**6.5);
+
+
+// Assignment operator
+
+// $a = 37;
+// $b = 5;
+// $a = $b;
+// $a += $b; // $a = $a + $b;
+// $a -= $b; // $a = $a - $b;
+// $a *= $b; // $a = $a * $b;
+// $a /= $b; // $a = $a / $b;
+// $a %= $b; // $a = $a % $b;
+// $a **= $b; // $a = $a ** $b;
+// echo $a;
+// echo $b;
+
+
+// Comparison Operators
+
+// $result = 12 < 15;
+// $result = 12.45 < 15.56;
+// $result = "hello" < "hello";
+// $result = "Hello" < "hello";
+// $result = true < false;
+// $result = true < null;
+// $result = false < null;
+
+// $result = 12 > 15;
+// $result = 12.45 > 15.56;
+// $result = "hello" > "hello";
+// $result = "hello world" > "hello";
+// $result = true > false;
+// $result = true > null;
+// $result = false > null;
+
+// $result = 12 >= 15;
+// $result = 12.45 >= 15.56;
+// $result = "hello" >= "hello";
+// $result = "hello " >= "hello";
+// $result = true >= false;
+// $result = true >= null;
+// $result = false >= null;
+
+// $result = 12 == 12;
+// $result = 12 == "12";
+// $result = 12.45 == 12.58;
+// $result = "hello12" == "hello";
+// $result = "Hello" == "hello";
+// $result = true == false;
+// $result = true == "true";
+// $result = true == null;
+// $result = false == null;
+
+// $result = 12 === 12;
+// $result = 12 === "12";
+// $result = 12.45 === 12.45;
+// $result = "hello123" === "hello";
+// $result = "hello" === "hello";
+// $result = true === false;
+// $result = true === "true";
+// $result = true === null;
+// $result = false === null;
+
+// $result = 12 != 15;
+// $result = 12 != "17";
+// $result = 12.45 != 12.45;
+// $result = "hello12" != "hello";
+// $result = "Hello" != "hello";
+// $result = true != false;
+// $result = true != "true";
+// $result = true != null;
+// $result = false != null;
+
+// $result = 12 !== 12;
+// $result = 12 !== "12";
+// $result = 12.45 !== 12.45;
+// $result = "hello123" !== "hello";
+// $result = "hello" !== "hello";
+// $result = true !== false;
+// $result = true !== "true";
+// $result = true !== null;
+// $result = false !== null;
+
+// echo $result;
+// var_dump($result);
+
+
+// Logical operator
+
+// $result = true && true;
+// $result = true && false;
+// $result = 15 && 0;
+// $result = "A" && "";
+
+// $result = true || true;
+// $result = true || false;
+// $result = 15 || 0;
+// $result = "A" || "K";
+// $result = false || false;
+
+// $result = !$result;
+
+// echo $result;
+// var_dump($result);
