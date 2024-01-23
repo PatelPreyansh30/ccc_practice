3f7d3426edbc43428281f4cfced1acbc426db234
Preyansh Patel <patelpreyansh3011@gmail.com>
2024-01-23 14:49:11 +0530

    odd and even pattern added

---
+++ assignment/pattern.php
@@ -0,0 +1,79 @@
+<?php
+
+$number = (int)readline("Enter number: ");
+
+function even_pattern($n1)
+{
+    for ($i = 1; $i <= $n1; $i++) {
+        for ($j = 1; $j < $i; $j++) {
+            echo "_";
+        };
+        for ($j = $i; $j <= $n1; $j++) {
+            echo "$j";
+        };
+        for ($j = 1; $j <= $n1 - $i + 1; $j++) {
+            echo $j + $n1;
+        };
+        for ($j = 1; $j < $i; $j++) {
+            echo "_";
+        };
+        echo "\n";
+    };
+    for ($i = 1; $i <= $n1 - 1; $i++) {
+        for ($j = 1; $j < $n1 - $i; $j++) {
+            echo "_";
+        };
+        for ($j = $n1 - $i; $j <= $n1; $j++) {
+            echo "$j";
+        };
+        for ($j = 1; $j <= $i + 1; $j++) {
+            echo $j + $n1;
+        };
+        for ($j = 1; $j < $n1 - $i; $j++) {
+            echo "_";
+        };
+        echo "\n";
+    };
+};
+
+function odd_pattern($n1)
+{
+    for ($i = 1; $i <= $n1; $i++) {
+        for ($j = 1; $j < $i; $j++) {
+            echo "_";
+        };
+        for ($j = $i; $j <= $n1; $j++) {
+            echo $j;
+        };
+        for ($j = 1; $j <= $n1 - $i; $j++) {
+            echo $j + $n1;
+        };
+        for ($j = 1; $j < $i; $j++) {
+            echo "_";
+        };
+        echo "\n";
+    };
+    for ($i = 1; $i <= $n1 - 1; $i++) {
+        for ($j = 1; $j < $n1 - $i; $j++) {
+            echo "_";
+        };
+        for ($j = $n1 - $i; $j <= $n1; $j++) {
+            echo "$j";
+        };
+        for ($j = 1; $j <= $i; $j++) {
+            echo $j + $n1;
+        };
+        for ($j = 1; $j < $n1 - $i; $j++) {
+            echo "_";
+        };
+        echo "\n";
+    };
+};
+
+if ($number % 2 == 0) {
+    even_pattern((int)$number / 2);
+} elseif ($number % 2 != 0) {
+    odd_pattern((int)ceil($number / 2));
+} else {
+    echo "Please enter valid number";
+};
