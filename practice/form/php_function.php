c49d3d4084f966e3b3bac1adbed81bcf01451fd0
Preyansh Patel <patelpreyansh3011@gmail.com>
2024-01-24 14:03:33 +0530

    insert, delete, update function

---
+++ practice/form/php_function.php
@@ -0,0 +1,33 @@
+<?php
+
+include('mysql_function.php');
+
+echo "<pre>";
+
+function getKeysFromPostRequest()
+{
+    $keys = [];
+    foreach ($_POST as $key => $val) {
+        $keys[] = $key;
+    };
+    return $keys;
+};
+
+function getPostData(string $key)
+{
+    return $_POST[$key];
+};
+
+function getGetData(string $key)
+{
+    return $_GET[$key];
+};
+
+$keys = getKeysFromPostRequest();
+
+for ($i = 0; $i < count($keys); $i++) {
+    $insert_query = insert($keys[$i], getPostData($keys[$i]));
+};
+
+
+// $update_query = update($tablename,);
