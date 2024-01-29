bc31122be5b66583dcf71dd479154fb3422e0e7e
Preyansh Patel <patelpreyansh3011@gmail.com>
2024-01-29 18:02:43 +0530

    practice class and objects

---
+++ practice/class/class_demo.php
@@ -0,0 +1,76 @@
+<?php
+
+echo "<pre>";
+
+// class Fruit
+// {
+//     public $name;
+    // public $color;
+//     protected $color;
+//     private $weigth;
+
+//     function __construct($name, $color, $weigth)
+//     {
+//         $this->name = $name;
+//         $this->color = $color;
+//         $this->weigth = $weigth;
+//     }
+
+    // function __destruct()
+    // {
+    //     echo "The fruit is {$this->name} and the color is {$this->color}";
+    // }
+
+    // function set_name($name, $color)
+    // {
+    //     $this->name = $name;
+    //     $this->color = $color;
+    // }
+
+    // function get_name()
+    // {
+    //     return $this->name;
+    // }
+// }
+
+// $mango = new Fruit("Mango", "Yellow", 8);
+// print_r($mango);
+
+// var_dump($mango->name); // NULL
+// var_dump($mango->get_name()); // NULL
+
+// $mango->set_name("Mango");
+// echo $mango->name; // Mango
+// echo $mango->get_name(); // Mango
+
+// $mango->name = "Mango";
+// echo $mango->name; // Mango
+// echo $mango->color;
+// echo $mango->weigth;
+
+// class A
+// {
+//     public $i = 0;
+//     public function inc()
+//     {
+//         $this->i++;
+//     }
+//     public function reset()
+//     {
+//         $this->i = 10;
+//     }
+// }
+
+// $obj1 = new A();
+// print_r($obj1);
+// $obj1->inc();
+// print_r($obj1);
+// $obj1->reset();
+// $obj1->i = 10;
+
+// $obj2 = new A();
+// $obj2->inc();
+
+// print_r($obj1);
+// $obj1->inc();
+// print_r($obj2);
