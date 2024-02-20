18aeece92e54c01291348e85d1681b4a71058b20
Preyansh Patel <patelpreyansh3011@gmail.com>
2024-02-20 11:23:19 +0530

    product and category from render

--- Project/app/code/local/Catalog/Controller/Product.php
+++ Project/app/code/local/Catalog/Controller/Product.php
@@ -1,5 +1,15 @@
 <?php
 
-class Catalog_Controller_Product
+class Catalog_Controller_Product extends Core_Controller_Front_Action
 {
+    public function newAction()
+    {
+        $layout = $this->getLayout();
+        // $layout->getChild('head')->addJs('abc.js');
+        $child = $layout->getChild('content');
+
+        $productForm = $layout->createBlock('catalog/admin_product');
+        $child->addChild('form', $productForm);
+        echo $layout->toHtml();
+    }
 }
--- Project/app/code/local/Catalog/Model/Product.php
+++ Project/app/code/local/Catalog/Model/Product.php
@@ -4,7 +4,7 @@ class Catalog_Model_Product extends Core_Model_Abstract
 {
     public function init()
     {
-        $this->_resourceClass = 'Product_Model_Resource_Product';
-        $this->_collectionClass = 'Product_Model_Resource_Collection_Product';
+        $this->_resourceClass = 'Catalog_Model_Resource_Product';
+        $this->_collectionClass = 'Catalog_Model_Resource_Collection_Product';
     }
 }
