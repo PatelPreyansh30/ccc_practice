f8c8504990508ca9dec5575ce27939be5090d9a1
Preyansh Patel <patelpreyansh3011@gmail.com>
2024-02-27 13:27:46 +0530

    js and css update

--- Project/skin/js/product/form.js
+++ Project/skin/js/product/form.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   setStatus();
+  setCategory();
+  // console.log(categoryId);
 });
 
 function setStatus() {
@@ -7,6 +9,15 @@ function setStatus() {
   for (let i = 0; i < status_element.length; i++) {
     if (status == status_element[i].innerHTML) {
       status_element[i].selected = "selected";
+    }
+  }
+}
+
+function setCategory() {
+  var category_element = document.getElementsByClassName("category");
+  for (let i = 0; i < category_element.length; i++) {
+    if (categoryId == category_element[i].value) {
+      category_element[i].selected = "selected";
     }
   }
 }
