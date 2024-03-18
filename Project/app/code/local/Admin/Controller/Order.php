f817d814765eb563a4fb95705f67e7d40339fee3
Preyansh Patel <patelpreyansh3011@gmail.com>
2024-03-18 17:53:52 +0530

    admin order view added

--- Project/app/code/local/Admin/Controller/Order.php
+++ Project/app/code/local/Admin/Controller/Order.php
@@ -14,4 +14,17 @@ class Admin_Controller_Order extends Core_Controller_Admin_Action
         $child->addChild('list', $adminList);
         $layout->toHtml();
     }
+    public function viewAction()
+    {
+        $layout = $this->getLayout();
+        $layout->getChild('head')
+            ->addCss('order/admin/view.css');
+        $child = $layout->getChild('content');
+
+        $adminView = $layout->createBlock('order/admin_view');
+
+        $child->addChild('view', $adminView);
+        $layout->toHtml();
+
+    }
 }
